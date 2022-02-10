307ad56bb0f3a9d4309a03740fe68c0bb442da7c
Ed Adkins <ejmadkins@gmail.com>
2022-02-10 15:43:43 +0000

    add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".nav-link").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav-link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
