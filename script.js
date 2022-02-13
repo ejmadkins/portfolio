fbac9d79f7b3b59e605dc9c1880cae5a2dd20504
Ed Adkins <ejmadkins@gmail.com>
2022-02-13 19:36:38 +0000

    change site colours

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const toggle = document.getElementById("theme-toggle");
+
 document.querySelector(".nav-link").addEventListener("click", function (e) {
   e.preventDefault();
 
@@ -7,3 +9,23 @@ document.querySelector(".nav-link").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+let storedTheme =
+  localStorage.getItem("theme") ||
+  (window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light");
+if (storedTheme)
+  document.documentElement.setAttribute("data-theme", storedTheme);
+
+toggle.onclick = function () {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  let targetTheme = "light";
+
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+  }
+
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+};
